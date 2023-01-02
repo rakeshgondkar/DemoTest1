643373b13a9ee7d57e0d76aa390f462a19197cbf
rakesh.g <rakesh.g@ATDEV283L1.Atyati.com>
2023-01-02 18:45:31 +0530

    Added the new condition to check is food ready or not

--- src/Test2.java
+++ src/Test2.java
@@ -5,6 +5,13 @@ public class Test2 {
 		// TODO Auto-generated method stub
 
 		System.out.println("This is newly added class");
+		
+		Boolean IsfoodReady = false;
+		if (IsfoodReady == true) {
+			System.out.println("Food is ready");
+		}else {
+			System.out.println("Food is not ready");
+		}
 	}
 
 }
