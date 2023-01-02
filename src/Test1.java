50b9bc87c6ee765159372b7dbd2321b46c33219b
rakesh.g <rakesh.g@ATDEV283L1.Atyati.com>
2023-01-02 13:32:15 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input = "Yes";
+		
+		if(input == "Yes") {
+			System.out.println("Success");
+		}else {
+			System.out.println("Fail");
+		}
+
+	}
+
+}
